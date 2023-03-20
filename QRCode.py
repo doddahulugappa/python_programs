7b4b6639c9f5775e82d4d744fa6f7d3a88589a35
huli <doddahulugappa@gmail.com>
2023-03-20 09:25:22 +0400

    QRCode.py QR Code prgram added

---
+++ QRCode.py
@@ -0,0 +1,14 @@
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('https://github.com/doddahulugappa/')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white", image_factory=StyledPilImage)
+img.save("qrcode.png")
