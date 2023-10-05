54b85873e7037d94b7d5e71f9ff64c35c59fe848
huli <doddahulugappa@gmail.com>
2023-10-05 09:44:23 +0400

    DrawHeart.py added using turtle library

---
+++ DrawHeart.py
@@ -0,0 +1,22 @@
+import math
+from turtle import *
+
+
+def heart_a(k):
+    return 15 * math.sin(k) ** 3
+
+
+def heart_b(k):
+    return 12 * math.cos(k) - 5 * \
+           math.cos(k * 2) - 2 * \
+           math.cos(k * 3) - \
+           math.cos(k * 4)
+
+
+speed(1000)
+bgcolor("black")
+
+for i in range(6000):
+    goto(heart_a(i) * 20, heart_b(i) * 20)
+    for j in range(5):
+        color("#f73487")
